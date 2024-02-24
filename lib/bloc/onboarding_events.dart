cecc4934a16a02afc8985487624e1c2fb45985ab
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2024-02-24 11:05:27 +0200

    Delete lib directory

--- lib/bloc/onboarding_events.dart
+++
@@ -1,3 +0,0 @@
-class OnboardingEvents {
-  OnboardingEvents(int value);
-}
--- lib/screens/auntification/login/interface/registration_strategy.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-
-/// Интерфейс для стратегии регистрации
-abstract class RegistrationStrategy {
-  Future<void> register(BuildContext context, String name, String email,
-      String password, File? image);
-}
